c10542f5dc2c25b8007e37457682184104aac017
gskielian <gregory.kielian@hackerdojo.com>
2013-10-16 11:02:18 -0700

    figured out on plunkr how to do live update of the d3 linechart
    Oh boy,this means that we're one quantized step from having a angular.js+d3+firebase

--- TimeTrials/AngularD3/PlunkrExample/app.js
+++ TimeTrials/AngularD3/PlunkrExample/app.js
@@ -1,18 +1,30 @@
 var app = angular.module('plunker', ['n3-charts.linechart']);
 
+
+
+
 app.controller('MainCtrl', function($scope) {
-  $scope.data = [{x: 0, value: 12}, {x: 1, value: 4}, {x: 2, value: 7}, {x: 3, value: 0}];
+
+  $scope.dasthing =0;
+  $scope.data = [{x: 0, value: $scope.dasthing}, {x: 1, value: 4}, {x: 2, value: 7}, {x: 3, value: 0}];
+  
+  
   
   // Line
   //$scope.options = {series: [{y: 'value', color: 'steelblue'}]};
   
   // Area
-  //$scope.options = {series: [{y: 'value', type: 'area', color: 'steelblue'}]};
+  //$scope.options = {series: [{y: 'value', tyep: 'area', color: 'steelblue'}]};
   
   // Column
   //$scope.options = {series: [{y: 'value', type: 'column', color: 'steelblue'}]};
   
   // Interpolation
   $scope.options = {lineMode: 'cardinal', series: [{y: 'value', color: 'steelblue'}]};
+  
+  $scope.change = function() {
+  $scope.data = [{x: 0, value: $scope.dasthing}, {x: 1, value: 4}, {x: 2, value: 7}, {x: 3, value: 0}];
+    
+  }
   
 });
