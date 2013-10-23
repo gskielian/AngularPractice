c05629d86029a83cde0640eaa50b7c84565141aa
gskielian <gregory.kielian@gmail.com>
2013-10-22 19:56:45 -0700

    added stuff

---
+++ TimeTrials/Directives/blog-post 4/blog-post-directives.js
@@ -0,0 +1,33 @@
+var app = angular.module("blogang", []);
+
+
+
+app.directive("greybig",function() {
+  return {
+    restrict:"EA",
+    scope: {
+      content:"@"
+    },
+    template: "<div>{{content}}</div>"
+  };
+});
+app.directive("lead",function() {
+  return {
+    restrict:"EA",
+    scope: {
+      content:"@"
+    },
+    template: "<p class=\"lead\">{{content}}</div>"
+  };
+});
+
+app.directive("navbar",function() {
+  return {
+    restrict:"EA",
+    scope: {
+      content:"@"
+    },
+    template: "<nav class=\"navbar navbar-inverse navbar-fixed-top\" role=\"navigation\"> <div class=\"container\"> <div class=\"navbar-header\"> <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\".navbar-ex1-collapse\"> <span class=\"sr-only\">toggle navigation</span> <span class=\"icon-bar\"></span> <span class=\"icon-bar\"></span> <span class=\"icon-bar\"></span> </button> <a class=\"navbar-brand\" href=\"~/Main/index.html\">G. S. Kielian Blog</a> </div> <!-- collect the nav links, forms, and other content for toggling --> <div class=\"collapse navbar-collapse navbar-ex1-collapse\"> <ul class=\"nav navbar-nav\"> <li><a href=\"#contact\">contact</a></li> </ul> </div><!-- /.navbar-collapse --> </div><!-- /.container --> </nav> "
+  };
+});
+
